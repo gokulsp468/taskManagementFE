cce52409628a47386c97c49e4a37375e39623312
gokulsp468 <gokul.s@spericorn.com>
2025-03-05 12:25:09 +0530

    style

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -3,5 +3,6 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 
 export const authRoutes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ];
