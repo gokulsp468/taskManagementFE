8cd1e7a23e7265274779cdca6bd1f7a9d977fc4a
vinayak-SP470 <vinayak.r@spericorn.com>
2025-03-05 16:36:56 +0530

    login page integration

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 export const authRoutes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent }
+
 ];
