80a6fa62f78d9b118d6b67f43b29bf74fc7ee23a
gokulsp468 <gokul.s@spericorn.com>
2025-03-05 16:31:50 +0530

    signup

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = environment.apiHost;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(userData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/auth/register/`, userData);
+  }
+
+
 }
