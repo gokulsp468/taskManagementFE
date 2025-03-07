9ef118ec4d1e700238e800749e5d43f02a2d7508
gokulsp468 <gokul.s@spericorn.com>
2025-03-07 17:13:44 +0530

    kanban board

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -1,11 +1,68 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  CdkDragHandle
+} from '@angular/cdk/drag-drop';
+
+
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule,CdkDrag, CdkDropList,CdkDragHandle],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+
+
+  columns = [
+    {
+      name: 'To Do',
+      tasks: [
+        { title: 'Product Design, Figma, Sketch', comments: 4, priority: 'Backlog', members: 5 },
+        { title: 'Create wireframe for homepage', comments: 2, priority: 'Urgent', members: 3 }
+      ]
+    },
+    {
+      name: 'In Progress',
+      tasks: [
+        { title: 'Develop API endpoints', comments: 3, priority: 'In Progress', members: 2 }
+      ]
+    },
+    {
+      name: 'Completed',
+      tasks: [
+        { title: 'Fix UI bugs in dashboard', comments: 1, priority: 'Completed', members: 4 }
+      ]
+    }
+  ];
+
+
+  dropTask(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+
+  drop(event: CdkDragDrop<any[]>) {
+    moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
+  }
+  // Function to handle column reordering
+  dropColumn(event: CdkDragDrop<any[]>) {
+    moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
+  }
 
 }
